bb7662d64a3a35f1caba7b2a6c60d4ca9479a0af
pollot-pierre-10antz <pollot.pierre@10antz.co.jp>
2025-05-13 14:58:55 +0900

    Revert "fix attempt no2"
    This reverts commit cc3b1d81448427e2497de331472b3c007483d03e.

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from argparse import ArgumentParser, Namespace
+
 from utils import mcp
 
 
+def parse_arguments() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument("--stdio", action="store_true")
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    mcp.run(transport="stdio")
+    args = parse_arguments()
+    
+    if args.stdio:
+        mcp.run(transport="stdio")
+        exit(0)
+        
+    mcp.run(transport="streamable-http")
