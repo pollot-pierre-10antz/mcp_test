cc3b1d81448427e2497de331472b3c007483d03e
pollot-pierre-10antz <pollot.pierre@10antz.co.jp>
2025-05-13 13:15:16 +0900

    fix attempt no2

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-from argparse import ArgumentParser, Namespace
-
 from utils import mcp
 
 
-def parse_arguments() -> Namespace:
-    parser = ArgumentParser()
-    parser.add_argument("--stdio", action="store_true")
-    return parser.parse_args()
-
-
 if __name__ == "__main__":
-    args = parse_arguments()
-    
-    if args.stdio:
-        mcp.run(transport="stdio")
-        exit(0)
-        
-    mcp.run(transport="streamable-http")
+    mcp.run(transport="stdio")
